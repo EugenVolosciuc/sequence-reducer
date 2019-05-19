55282b9fe8e133824c6250a978e4210c535c389b
Eugen Volosciuc <volosciuc.eugen@gmail.com>
2019-05-19 12:57:12 +0300

    Fixed bug where adding regexp characters as deleting points crashed the app

--- app.js
+++ app.js
@@ -8,29 +8,34 @@ const inputTo = document.querySelector("#to");
 let from;
 let to;
 
+function regExpEscape(literal_string) {
+    return literal_string.replace(/[-[\]{}()*+!<=:?.\/\\^$|#\s,]/g, '\\$&');
+}
+
 function parseText() {
     const text = document.querySelector("#input-area").value;
     var lines = text.split("\n");
     var result = '';
-    let regexp = new RegExp(`^(>)(.*${from} )(.*)( ${to}.*)$`);
-
+    let regexp = new RegExp(`^(>)(.*?${from})(.*)(${to}.*)$`);
     for (var i in lines) {
         result += lines[i].replace(regexp, "$1$3") + "\n";
     }
-
     document.querySelector('#output-area').innerText = result;
     inputFrom.textContent = '';
     inputTo.textContent = '';
 }
 
 setFrom.addEventListener("click", () => {
-    from = new RegExp(inputFrom.value).toString().slice(1, -1);
-    outputFrom.textContent = "Delete until: " + from;
+    from = new RegExp(regExpEscape(inputFrom.value)).toString().slice(1, -1);
+    console.log(from);
+    outputFrom.textContent = "Delete until: " + from.toString().replace("\\", "");
 })
 
 setTo.addEventListener("click", () => {
-    to = new RegExp(inputTo.value).toString().slice(1, -1);
-    outputTo.textContent = "Delete from: " + to;
+    to = new RegExp(regExpEscape(inputTo.value)).toString().slice(1, -1);
+    console.log(to);
+    outputTo.textContent = "Delete from: " + to.toString().replace("\\", "");
 })
+
 
 submit.addEventListener("click", parseText);
